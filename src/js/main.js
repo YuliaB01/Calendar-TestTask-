baea082a831aa1675a5b84c8883517e5728e78de
Yulia Bilash <starlight05032015@gmail.com>
2017-04-12 23:02:45 +0300

    Added table body with days

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,21 @@
 function createCalendar(id, year, month) {
-    var calendar = document.getElementById(id);
-    calendar.appendChild(createCalendarHeader(year, month));
-    calendar.appendChild(createTable());
+    var calendarElement = document.getElementById(id);
+    calendarElement.appendChild(createCalendarHeader(year, month));
+    calendarElement.appendChild(createTable(year, month));
 }
-//creates the whole table
-function createTable() {
+
+// creates the whole table
+function createTable(year, month) {
     var table = document.createElement("table");
     table.className = "table";
 
     table.appendChild(createTableHead());
+    table.appendChild(createTableBody(year, month));
 
     return table;
 }
-//creates head of the calendar with month name and year
+
+// creates head of the calendar with month name and year
 function createCalendarHeader(year, month) {
     var monthName = new Date(year, --month).toLocaleString("en-us", {month: "long"});//shows month name in a full form
 
@@ -40,7 +43,8 @@ function createCalendarHeader(year, month) {
 
     return head;
 }
-//creates row with day names
+
+// creates row with day names
 function createTableHead() {
     var row = document.createElement("tr");
     row.className = "th-row";
@@ -48,7 +52,7 @@ function createTableHead() {
     var dayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
     for(var i = 0; i < dayNames.length; i++) {
-        var td = document.createElement("td");
+        var td = document.createElement("th");
         td.className = "days";
         td.textContent = dayNames[i];
         row.appendChild(td);
@@ -57,5 +61,43 @@ function createTableHead() {
     return row;
 }
 
-createCalendar("cal", 2011, 2);
+// creates the whole table with days
+function createTableBody(year, month) {
+    var date = new Date(year, --month),
+        curMonth = date.getMonth(), // current month
+        curYear = date.getFullYear(), // current year
+        lastMonthDay = new Date(curYear, curMonth + 1, 0).getDate(), // last day of the month
+        firstMonthDay = new Date(curYear, curMonth, 1); // first day of the month
+
+    var weekDay = firstMonthDay.getDay() - 1; // number of the first month day in the week
+    if(weekDay < 0) {
+        weekDay = 6;
+    }
+
+    var tbody = document.createElement("tbody");
+    var tr = document.createElement("tr");
+
+    for(var i = 1, j = 1; i <= lastMonthDay + weekDay; i++) {
+        var td = document.createElement("td");
+        if(i < weekDay) {
+            td.textContent = "";
+        } else {
+            td.textContent = j++;
+        }
+        tr.appendChild(td);
+
+        if(i % 7 === 0) {
+            tbody.appendChild(tr);
+            var tr = document.createElement("tr");
+        }
+
+        if(i === lastMonthDay) {
+            tbody.appendChild(tr);
+        }
+    }
+
+    return tbody;
+}
+
+createCalendar("cal", 2017, 2);
 
